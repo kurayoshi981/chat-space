8559a093ca2bd01a6216172dc23fd307ce50e528
kurayoshi981 <tachp981@yahoo.co.jp>
2019-01-20 17:40:58 +0900

    Haml,saccを直す前

---
+++ app/assets/javascripts/message.js
@@ -0,0 +1,49 @@
+$(function(){
+  function buildHTML(message){
+    var html = ` <div class="main__body-message">
+                    <div class="main__body-message-name">
+                      ${message.user_name}
+                    </div>
+
+                    <div class="main__body-message-time">
+                      ${message.created_at}
+                    </div>
+                    <div class="main__body-message-body">
+                      <p class="lower-message__content">${message.content }</p>
+                    </div>
+                  </div>`
+    return html;
+  }
+
+  $('.new_message').on('submit', function(e){
+    e.preventDefault();
+    var formData = new FormData(this);
+    var url = $(this).attr('action')
+    $.ajax({
+      url: url,
+      type: "POST",
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
+    .done(function(data){
+      var html = buildHTML(data);
+      $('.messages').append(html);
+    })
+    .fail(function(){
+      alert('error');
+    })
+  })
+});
+
+
+
+
+
+
+
+
+
+
+
